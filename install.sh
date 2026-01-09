3c3903e7e1c883c2b07e70e89e247d6091935045
Sherman Devers <67293014+ShermanDevers@users.noreply.github.com>
2026-01-09 15:28:43 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #Install packages.nix to appropriate location
-sudo wget "https://raw.githubusercontent.com/ShermanDevers/dotfiles/refs/heads/main/nixConfig/packages.nix" -O /etc/nixos/;
+sudo wget "https://raw.githubusercontent.com/ShermanDevers/dotfiles/refs/heads/main/nixConfig/packages.nix" -O /etc/nixos/packages.nix;
 echo "Downloaded packages.nix";
 
-sudo wget "https://raw.githubusercontent.com/ShermanDevers/dotfiles/refs/heads/main/nixConfig/fonts.nix" -O /etc/nixos/;
+sudo wget "https://raw.githubusercontent.com/ShermanDevers/dotfiles/refs/heads/main/nixConfig/fonts.nix" -O /etc/nixos/fonts.nix;
 echo "Downloaded fonts.nix";
 
 read -n 1 -p "Please include the new files in configuration.nix, then press anything to continue";
