956dfac9e4548a3913bac2429c2b6e9ccce8925d
Sherman Devers <67293014+ShermanDevers@users.noreply.github.com>
2026-01-31 11:13:16 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,17 @@
 sudo wget "https://raw.githubusercontent.com/ShermanDevers/dotfiles/refs/heads/main/nixConfig/packages.nix" -O /etc/nixos/packages.nix;
 echo "Downloaded packages.nix";
 
+#Same but with fonts
 sudo wget "https://raw.githubusercontent.com/ShermanDevers/dotfiles/refs/heads/main/nixConfig/fonts.nix" -O /etc/nixos/fonts.nix;
 echo "Downloaded fonts.nix";
 
 read -n 1 -p "Please include the new files in configuration.nix, then press anything to continue"; 
 sudo nixos-rebuild switch
+
+#Install vim-plug
+rm -rf ~/.config/nvim
+git clone https://github.com/ShermanDevers/LazyConf/ ~/.config/nvim
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
